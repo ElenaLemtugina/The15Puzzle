8f0c75c73de2211f08cbdd97c13cb0ea9c5bf8ae
Elena Lemtugina <nuggetnuggetson@gmail.com>
2017-03-14 11:51:30 +0500

    Update Game.cs

--- The15Puzzle/Game.cs
+++ The15Puzzle/Game.cs
@@ -116,7 +116,7 @@ namespace The15Puzzle
             for (int i = 0; i < (int)Math.Sqrt(fieldMatrix.Length); i++)
             {
                 for (int j = 0; j < (int)Math.Sqrt(fieldMatrix.Length); j++)
-                    Console.Write(fieldMatrix[i, j] + " ");
+                    Console.Write(fieldMatrix[i, j] + "\t ");
                 Console.WriteLine();
             }
         }
