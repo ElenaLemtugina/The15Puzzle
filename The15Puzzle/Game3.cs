fde4a4c0f017f7f7d7c8d3ede6d29416b7958985
Elena Lemtugina <nuggetnuggetson@gmail.com>
2017-03-28 13:08:57 +0500

    Create Game3.cs

---
+++ The15Puzzle/Game3.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication68
+{
+    class Game3
+    {
+        class Game3 : Game2
+        {
+            private List<int> moves;
+            private int lastMoveIndex;
+            public Game3(params int[] blocks) : base(blocks)
+            {
+                moves = new List<int>();
+                lastMoveIndex = -1;
+            }
+
+            public void Back()
+            {
+                if (lastMoveIndex != -1)
+                {
+                    base.Shift(moves[lastMoveIndex--]);
+                }
+            }
+
+            public void Forward()
+            {
+                if (moves.Count() - 1 != lastMoveIndex && moves.Count() != 0)
+                {
+                    base.Shift(moves[++lastMoveIndex]);
+                }
+            }
+
+            public override void Shift(int value)
+            {
+                base.Shift(value);
+                moves.Add(value);
+            }
+
+        }
+    }
+}
